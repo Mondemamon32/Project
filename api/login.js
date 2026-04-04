b0c38bba3028e69d2b29bbe4a877ff0ae3f5fce9
Mondemamon32 <raymond_bool@dlsu.edu.ph>
2026-04-04 21:38:48 +0800

    new 2

--- api/login.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const user = JSON.parse(req.body);
-
-  if(user.email && user.password) {
-    res.status(200).json({ success: true });
-  } else {
-    res.status(401).json({ error: "Invalid" });
-  }
-}
